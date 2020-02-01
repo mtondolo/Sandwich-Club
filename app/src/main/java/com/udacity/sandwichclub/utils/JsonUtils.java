4665c0009a4227dd3fabeb0d6bf2afcf2a381f9d
mtondolo <mtondolo@gmail.com>
2020-02-01 11:01:15 +0200

    Parse json string

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,36 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Collections;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject sandwichObject = new JSONObject(json);
+
+            JSONObject nameJsonObject = sandwichObject.getJSONObject("name");
+            String mainName = nameJsonObject.getString("mainName");
+            List<String> alsoKnownAs = Collections.singletonList(nameJsonObject.getString("alsoKnownAs"));
+
+            String placeOfOrigin = sandwichObject.getString("placeOfOrigin");
+            String description = sandwichObject.getString("description");
+            String image = sandwichObject.getString("image");
+            List<String> ingredients = Collections.singletonList(sandwichObject.getString("ingredients"));
+
+            Sandwich sandwichItem = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description,
+                    image, ingredients);
+
+            sandwich = sandwichItem;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
 }
